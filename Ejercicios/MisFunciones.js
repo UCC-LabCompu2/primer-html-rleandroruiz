10707376413ea852f360b6d97b42604d0f4063d8
rleandroruiz <leo1998.04.24@hotmail.com>
2017-05-04 09:45:37 -0300

    Hola Mundo en Javascript

---
+++ Ejercicios/MisFunciones.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Alumno on 04/05/2017.
+ */
+/**
+ * Sumatoria de dos números
+ * @method calcular
+ * @param num1
+ * @param num2
+ * @return total
+ */
+
+Function calcular(num1, num2) {
+    var total;
+    total = num1 + num2;
+    
+    return total;
+}
